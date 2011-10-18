3091d153231e5872941bb53657404e316baca1e8
Steven E. Bailey <github@leidentech.com>
2011-10-18 18:10:41 +0200

    Adding translations.  Not sure how the rest got changed.

---
+++ translations/dut-NL/translation.ts
@@ -0,0 +1,214 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>design/exam</name>
+    <message>
+        <source>Start Exam</source>
+        <translation>Begin Examen</translation>
+    </message>
+    <message>
+        <source>id</source>
+        <translation>id</translation>
+    </message>
+    <message>
+        <source>title</source>
+        <translation>titel</translation>
+    </message>
+    <message>
+        <source>number of testees</source>
+        <translation>antaal deelnemers</translation>
+    </message>
+    <message>
+        <source>pass on first try</source>
+        <translation>Geslaagd of de eerste poging</translation>
+    </message>
+    <message>
+        <source>pass on second try</source>
+        <translation>geslaagd op de tweede poging</translation>
+    </message>
+    <message>
+        <source>High score</source>
+        <translation>hoge score</translation>
+    </message>
+    <message>
+        <source>text</source>
+        <translation>text</translation>
+    </message>
+    <message>
+        <source>total answered</source>
+        <translation>totaal beantwoord</translation>
+    </message>
+    <message>
+        <source>correct on first try</source>
+        <translation>goed op de eerste poging</translation>
+    </message>
+    <message>
+        <source>correct on second try</source>
+        <translation>goed op de tweede poging</translation>
+    </message>
+    <message>
+        <source>question</source>
+        <translation>vraag</translation>
+    </message>
+    <message>
+        <source>total</source>
+        <translation>totaal</translation>
+    </message>
+    <message>
+        <source>answer</source>
+        <translation>antwoord</translation>
+    </message>
+    <message>
+        <source>Times chosen</source>
+        <translation>Keer gekozen</translation>
+    </message>
+    <message>
+        <source>Exam not found.</source>
+        <translation>Examen neit gevonden.</translation>
+    </message>
+    <message>
+        <source>No exams have been created yet.</source>
+        <translation>Geen examens zijn nog niet aangemaakt.</translation>
+    </message>
+    <message>
+        <source>group</source>
+        <translation>groep</translation>
+    </message>
+    <message>
+        <source>Use these buttons to move elements up or down</source>
+        <translation>Gebruik de rangschikking knoppen om de rangschikking van elementen te bepalen.</translation>
+    </message>
+    <message>
+        <source>pagebreak</source>
+        <translation>nieuwe pagina</translation>
+    </message>
+    <message>
+        <source>if picked remove</source>
+        <translation>Indien gekozen verwijder</translation>
+    </message>
+    <message>
+        <source>if picked add</source>
+        <translation>Indien gekozen voeg toe</translation>
+    </message>
+    <message>
+        <source>if picked follow with</source>
+        <translation>Indien gekozen volg met</translation>
+    </message>
+    <message>
+        <source>if picked display in results</source>
+        <translation>Indien gekozen weergave van de resultaten</translation>
+    </message>
+    <message>
+        <source>if not picked remove</source>
+        <translation>Indien niet gekozen verwijder</translation>
+    </message>
+    <message>
+        <source>if not picked add</source>
+        <translation>Indien niet gekozen voeg toe</translation>
+    </message>
+    <message>
+        <source>if not picked follow with</source>
+        <translation>Indien niet gekozen volg met</translation>
+    </message>
+    <message>
+        <source>if not picked display in results</source>
+        <translation>Indien niet gekozen weergave van de resultaten</translation>
+    </message>
+    <message>
+        <source>You passed</source>
+        <translation>Je bent geslaagd</translation>
+    </message>
+    <message>
+        <source>on your second try.</source>
+        <translation>op uw tweede poging.</translation>
+    </message>
+    <message>
+        <source>You failed</source>
+        <translation>Je hebt gefaald</translation>
+    </message>
+    <message>
+        <source>Do you want to try again?</source>
+        <translation>Wilt u het opnieuw proberen?</translation>
+    </message>
+    <message>
+        <source>Restart Exam</source>
+        <translation>Herstart Examen</translation>
+    </message>
+    <message>
+        <source>That was your second attempt.  Study harder and try again some other day.</source>
+        <translation>Dat was uw tweede poging. Studie harder en weer een andere dag te proberen.</translation>
+    </message>
+    <message>
+        <source>Your score was [score]% correct</source>
+        <comment>score</comment>
+        <translation>Uw score was [score]% correct</translation>
+    </message>
+    <message>
+        <source>You were one of [examCount] to take this exam.  Of which [passFirst] passed.</source>
+        <comment>score</comment>
+        <translation>Je was een van de [examCount] om dit examen te nemen. (Sp) (sp) Waarvan [passFirst] zijn geslaagd.</translation>
+    </message>
+    <message>
+        <source>on the first try and [passSecond] passed on the second try.</source>
+        <comment>score</comment>
+        <translation>op de eerste poging en [passSecond] op de tweede poging.</translation>
+    </message>
+    <message>
+        <source>The highest score recorded was [highScore].</source>
+        <comment>score</comment>
+        <translation>De beste score was [highScore].</translation>
+    </message>
+    <message>
+        <source>Congratulations, you got the high score.</source>
+        <translation>Gefeliciteerd, je hebt de hoge score.</translation>
+    </message>
+    <message>
+        <source>Do you wish to have a certificate to commemorate your success?</source>
+        <translation>Wilt u een certificaat voor uw succes te herdenken hebben?</translation>
+    </message>
+    <message>
+        <source>Download Certificate</source>
+        <translation>Download Certificaat</translation>
+    </message>
+    <message>
+        <source>I passed the exam at [url]</source>
+        <comment>url</comment>
+        <translation></translation>
+    </message>
+    <message>
+        <source>ERROR</source>
+        <translation>FOUT</translation>
+    </message>
+    <message>
+        <source>Cookies must be enabled to participate.</source>
+        <translation>Cookies moeten aanstaan ​​om deel te nemen.</translation>
+    </message>
+    <message>
+        <source>No exam id found.</source>
+        <translation>Geen examen id gevonden.</translation>
+    </message>
+    <message>
+        <source>No exam language found.</source>
+        <translation>Geen examen taal gevonden.</translation>
+    </message>
+    <message>
+        <source>No exam version found.</source>
+        <translation>Geen examen versie gevonden.</translation>
+    </message>
+    <message>
+        <source>No hash found.</source>
+        <translation>Geen hash gevonden.</translation>
+    </message>
+    <message>
+        <source>No such object.</source>
+        <translation>Een dergelijk object.</translation>
+    </message>
+    <message>
+        <source>Wrong object type.  Check your exam id.</source>
+        <translation>Verkeerd object type. (Sp) (sp) Controleer uw examen id.</translation>
+    </message>
+    <message>
+        <source>You&apos;ve tried to many times for today.  Try again on another day.</source>
+        <translation>Je hebt te veel keer vandaag geprobeerd. (Sp) (sp) Probeer het opnieuw op een andere dag.</translation>
+    </message>
+</context>
+</TS>
